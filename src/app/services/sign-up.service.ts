c4a9449cee6daf7780dde4cf6003500a0bb86e1d
Alejandra <ortizalejamedina@gmail.com>
2024-07-07 18:07:47 -0500

    get user from token

--- src/app/services/sign-up.service.ts
+++ src/app/services/sign-up.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Person } from 'src/environments/types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignUpService {
-  private apiUrl = 'http://localhost:8089/auth/register'; 
+  private apiUrl = 'http://localhost:8089/auth'; 
 
   constructor(private http: HttpClient) { }
 
   register(data: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}`, data);
+    return this.http.post(`${this.apiUrl}/register`, data);
+  }
+  getUser(token: string): Observable<Person> {
+    const headers = new HttpHeaders({'token': `${token}`});
+    return this.http.get<Person>(`${this.apiUrl}/user`, { headers });
   }
 }
